43bb46b22b3b0de2fb00a86eeba930091d82778c
DwiSetiawati <dwisetwa27@gmail.com>
2025-06-05 11:42:05 +0700

    membuat pernyataan else

--- stack.cpp
+++ stack.cpp
@@ -62,3 +62,15 @@ public:
         {
             cout << "List is empty." << endl;
         }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        } // return the value of the top node
+    }
+};
