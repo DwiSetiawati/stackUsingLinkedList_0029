9268a32f2d84f331126ef15f20e7d9c73180a67f
DwiSetiawati <dwisetwa27@gmail.com>
2025-06-05 11:38:39 +0700

    membuat fungsi peek-mengambil nilai topmost tanpa menghapusnya

--- stack.cpp
+++ stack.cpp
@@ -54,4 +54,6 @@ public:
         cout << "Popped value: " << top->data << endl;
         top = top->next; // update the top pointer to the next node
     }
-};
+
+    // peek/top operation: retrieve the value of the topmost element without removing it
+    void peek()
